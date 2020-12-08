bc0d0f48049fbcb33494826055347da82158d81a
Uwem Israel <uwemneku@gmail.com>
2020-12-09 00:41:20 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let text = document.getElementsByTagName('p');
-textToWrite = 'EFFICATIVE';
-function writeText (a){
-    text[0].innerText += a;
-}
-
-document.querySelector('video').playbackRate = 0.70;
-document.querySelector('video').loop = "true";
-document.querySelector('video').play();
